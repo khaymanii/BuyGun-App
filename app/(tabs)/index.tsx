506394630e8abe29f0dca131abcd1e29f0149a76
khaymanii <kennethakpo61@gmail.com>
2024-12-30 23:34:57 +0100

    created hero component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,11 @@
+import Hero from "@/components/Hero";
 import { View, Text, ScrollView } from "react-native";
 import tw from "twrnc";
 
 export default function index() {
   return (
     <ScrollView style={tw`bg-white`}>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
-      <View>text</View>
+      <Hero />
     </ScrollView>
   );
 }
