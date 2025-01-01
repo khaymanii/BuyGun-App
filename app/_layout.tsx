05b304f569658c93259a35e70ffc7ac445d8fc0b
khaymanii <kennethakpo61@gmail.com>
2025-01-01 10:38:31 +0100

    tried setting up fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,20 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import tw from "twrnc";
 import Header from "@/components/Header";
+import { AuthProvider } from "@/context/AuthContext";
+import { CartProvider } from "@/context/CartContext";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    PoppinsRegular: require("../assets/fonts/Poppins-Regular.ttf"),
+    PoppinsBold: require("../assets/fonts/Poppins-Bold.ttf"),
+    PoppinsMedium: require("../assets/fonts/Poppins-Medium.ttf"),
+    PoppinsLight: require("../assets/fonts/Poppins-Light.ttf"),
+    PoppinsSemiBold: require("../assets/fonts/Poppins-SemiBold.ttf"),
+    PoppinsBlack: require("../assets/fonts/Poppins-Black.ttf"),
+    PoppinsExtraBold: require("../assets/fonts/Poppins-ExtraBold.ttf"),
   });
 
   useEffect(() => {
@@ -25,48 +33,52 @@ export default function RootLayout() {
   }
 
   return (
-    <View style={tw`flex-1`}>
-      <Header />
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="home"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="signup"
-          options={{
-            headerShown: false,
-          }}
-        />{" "}
-        <Stack.Screen
-          name="signin"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="products"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="checkout"
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen name="not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </View>
+    <AuthProvider>
+      <CartProvider>
+        <View style={tw`flex-1`}>
+          <Header />
+          <Stack>
+            <Stack.Screen
+              name="(tabs)"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="home"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="signup"
+              options={{
+                headerShown: false,
+              }}
+            />{" "}
+            <Stack.Screen
+              name="signin"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="products"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="checkout"
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen name="not-found" />
+          </Stack>
+          <StatusBar style="auto" />
+        </View>
+      </CartProvider>
+    </AuthProvider>
   );
 }
